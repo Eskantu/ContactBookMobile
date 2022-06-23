1c9d67f8724a3d283e1bcc4bc03f79cb8c20b94e
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-06-23 18:16:11 -0500

    Add: Int to color converter 1=green, 0=Red

---
+++ ContactBookMobile/ContactBookMobile/Helpers/Converters/IntToColor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ContactBookMobile.Helpers.Converters
+{
+    public class IntToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)value) == 1 ? Color.Green : Color.Red;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((Color)value)==Color.Red ? 0 : 1;
+        }
+    }
+}
