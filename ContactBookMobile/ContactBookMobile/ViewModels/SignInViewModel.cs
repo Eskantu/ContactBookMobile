89214aaa516e19f1faaf23f80e254ae618854680
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-06-23 11:57:12 -0500

    Add: SignInViewModel

---
+++ ContactBookMobile/ContactBookMobile/ViewModels/SignInViewModel.cs
@@ -0,0 +1,20 @@
+﻿using ContactBookMobile.Services.Navigation;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace ContactBookMobile.ViewModels
+{
+   public class SignInViewModel:BaseViewModel
+    {
+        public ICommand BackLoginCommand { get; set; }
+        public SignInViewModel(INavigationService navigation)
+        {
+            BackLoginCommand = new Command(async () => await navigation.GoBack());
+        }
+    }
+}
