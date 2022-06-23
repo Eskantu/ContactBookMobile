bc9cef7c5f1b09b3d2f26382fbac69ea763aa68d
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-06-23 11:56:31 -0500

    Add: UserFormView

---
+++ ContactBookMobile/ContactBookMobile/Views/Formularios/UserFormView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ContactBookMobile.Views.Formularios
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserFormView : ContentView
+    {
+        public UserFormView()
+        {
+            InitializeComponent();
+        }
+    }
+}
