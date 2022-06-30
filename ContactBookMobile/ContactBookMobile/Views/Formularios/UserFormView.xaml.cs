ac03409b775a990347ecc0c3d5d01dce4407b1d1
Mario Escalante Cantu <ma.escalante@mavi.mx>
2022-06-30 18:21:08 -0500

    Style: creacion de button style block para editar/crear usaurio

--- ContactBookMobile/ContactBookMobile/Views/Formularios/UserFormView.xaml.cs
+++ ContactBookMobile/ContactBookMobile/Views/Formularios/UserFormView.xaml.cs
@@ -12,9 +12,11 @@ namespace ContactBookMobile.Views.Formularios
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class UserFormView : ContentView
     {
+        public static readonly BindableProperty isExpandedButtonProperty = BindableProperty.Create(nameof(IsExpandedButton), typeof(bool), typeof(UserFormView), false);
         public UserFormView()
         {
             InitializeComponent();
         }
+        public bool IsExpandedButton { get => (bool)GetValue(isExpandedButtonProperty); set => SetValue(isExpandedButtonProperty, value); }
     }
 }
